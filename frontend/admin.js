8342a42900fc9a976a06d73ce3fe4772a5eb5745
ngxhung <ndh171106hung@gmail.com>
2026-06-02 06:50:55 +0700

    Update admin.js

--- frontend/admin.js
+++ frontend/admin.js
@@ -12,7 +12,7 @@
 const API =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
-    ? "http://localhost:3000/api"
+    ? "https://naphbook-com.onrender.com/api"
     : "/api";
 
 let db = {
